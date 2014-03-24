16de478a9e56b388a87a87a068d5f18553484d85
Jose R. Borja <chakaitos@gmail.com>
2014-03-24 14:30:14 -0500

    Add get and post routes

--- server.rb
+++ server.rb
@@ -6,3 +6,25 @@ set :bind, '0.0.0.0' # This is needed for Vagrant
 get '/' do
   "Hey, this is a web app"
 end
+
+get '/hello' do
+	"Hello friends!"
+end
+
+get '/users' do
+	"This will be the users' index"
+end
+
+get '/members' do
+	"This will be the members' index"
+end
+
+post '/entries' do
+  '''
+  <form method="post">
+  	<input type="text" name="username"></input>
+  	<input type="password" name="password"></input>
+  	<button type="submit" value="submit"></button>
+  </form>
+  '''
+end
